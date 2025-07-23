47c6c6ab48449a23dfc1d8695b19769026552160
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-23 15:19:09 -0400

    Write attention profile script

---
+++ cs336_systems/pytorch_attention.py
@@ -0,0 +1,29 @@
+from cs336_basics.optimizer import AdamW
+from cs336_basics.nn_utils import cross_entropy
+from cs336_basics.data import get_batch
+from cs336_basics.model import scaled_dot_product_attention
+import torch
+import numpy as np
+from torch.profiler import profile, ProfilerActivity, record_function
+
+batch_size = 8
+d_models = [16, 32, 64, 128]
+seq_lens = [256, 1024, 4096, 8192, 16384]
+
+for d_model in d_models:
+    for seq_len in seq_lens:
+        Q, K, V = torch.randn((3, batch_size, seq_len, seq_len))
+        with torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                # torch.profiler.ProfilerActivity.CUDA,
+            ],
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler("./log/transformer"),
+            record_shapes=True,
+            profile_memory=True,
+            with_stack=True,
+        ) as prof:
+            for _ in range(102):
+                logits = scaled_dot_product_attention(Q, K, V)
+                prof.step()
