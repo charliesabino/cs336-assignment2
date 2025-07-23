36fdf8b1056f9008f1f6581108acc055a279a09b
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-23 16:40:59 -0400

    Upd

--- cs336_systems/pytorch_attention.py
+++ cs336_systems/pytorch_attention.py
@@ -1,12 +1,7 @@
-from cs336_basics.optimizer import AdamW
-from cs336_basics.nn_utils import cross_entropy
-from cs336_basics.data import get_batch
 from cs336_basics.model import scaled_dot_product_attention
 import torch
-import numpy as np
-from torch.profiler import profile, ProfilerActivity, record_function
-import datetime
-import os
+import numpy as numpy
+import record_function
 
 batch_size = 8
 d_models = [16, 32, 64, 128]
@@ -15,20 +10,22 @@ seq_lens = [256, 1024, 4096, 8192, 16384]
 for d_model in d_models:
     for seq_len in seq_lens:
         print(f"Profiling d_model={d_model}, seq_len={seq_len}")
-        Q, K, V = torch.randn((3, batch_size, seq_len, d_model), requires_grad=True).cuda()
+        Q, K, V = torch.randn(
+            (3, batch_size, seq_len, d_model), requires_grad=True).cuda()
 
         with torch.profiler.profile(
             activities=[
                 torch.profiler.ProfilerActivity.CPU,
                 torch.profiler.ProfilerActivity.CUDA,
             ],
-            schedule=torch.profiler.schedule(wait=1, warmup=1, active=100),
+            schedule=torch.profiler.schedule(
+                wait=1, warmup=1, active=3, repeat=2),
             on_trace_ready=torch.profiler.tensorboard_trace_handler("./log"),
             record_shapes=True,
             profile_memory=True,
             with_stack=True,
         ) as prof:
-            for i in range(102):
+            for i in range(10):
                 if i % 20 == 0:
                     print(f"  Progress: {i}/102")
 
@@ -45,3 +42,5 @@ for d_model in d_models:
                 V.grad = None
 
                 prof.step()
+
+        print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
