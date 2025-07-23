b346472f731f51068ce1f18fea97eec0fbc8c988
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-23 15:31:40 -0400

    Add logging

--- cs336_systems/pytorch_attention.py
+++ cs336_systems/pytorch_attention.py
@@ -12,18 +12,21 @@ seq_lens = [256, 1024, 4096, 8192, 16384]
 
 for d_model in d_models:
     for seq_len in seq_lens:
+        print(f"Profiling d_model={d_model}, seq_len={seq_len}")
         Q, K, V = torch.randn((3, batch_size, seq_len, seq_len))
         with torch.profiler.profile(
             activities=[
                 torch.profiler.ProfilerActivity.CPU,
-                # torch.profiler.ProfilerActivity.CUDA,
+                torch.profiler.ProfilerActivity.CUDA,
             ],
-            schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+            schedule=torch.profiler.schedule(wait=1, warmup=1, active=100),
             on_trace_ready=torch.profiler.tensorboard_trace_handler("./log/transformer"),
             record_shapes=True,
             profile_memory=True,
             with_stack=True,
         ) as prof:
-            for _ in range(102):
+            for i in range(102):
+                if i % 20 == 0:
+                    print(f"  Progress: {i}/102")
                 logits = scaled_dot_product_attention(Q, K, V)
                 prof.step()
