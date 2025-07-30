a426c9bcdc2fcdbde2cdb24a9eda8d475824aef1
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-30 11:09:11 -0400

    Begin dist

---
+++ cs336_systems/distributed.py
@@ -0,0 +1,33 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+
+def setup(rank: int, world_size: int) -> None:
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def distributed_demo(rank: int, world_size: int) -> None:
+    print(f"Starting rank {rank} of {world_size}...")
+    setup(rank, world_size)
+
+    data = torch.randint(0, 10, (3,), dtype=torch.int64)
+    print(f"Rank {rank} data (before all-reduce): {data}")
+
+    dist.all_reduce(data)
+
+    print(f"Rank {rank} data (after all-reduce):  {data}")
+
+
+def main():
+    world_size = 4
+    print(f"Spawning {world_size} processes...")
+    mp.spawn(fn=distributed_demo, args=(
+        world_size,), nprocs=world_size, join=True)
+
+
+if __name__ == "__main__":
+    main()
